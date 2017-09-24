c58e3626d483df92ed333c354898163fd8a99c2f
Eric Rogers <rogersgeric@gmail.com>
2017-09-24 17:50:24 -0400

    Add files via upload
    updated version of project

--- first.c
+++ first.c
@@ -17,14 +17,31 @@ int main (int argc, char * argv[]){
 	char str[60]; // this might need to be bigger, depending on # used 
 	char* token; 
 	char* data;
+	int actual_Data;
+	Node* head;
+	Node* new_Node;
 	while( fgets( str, 60, fp)){	//Read in information line by line 
 		//granted, check if 60 is large enough 
 		token = strtok(str, " ");
 		data = strtok(NULL, " ");
 		if(*token == 'i'){
 			//need to insert / create node
+			actual_data = atoi(data); // convert data to int
+
+			if(head == NULL){
+				//Linked list empty
+				head = create( actual_data , NULL); // first item in Linked List
 			
-		}else if (str[0] == 'd'){
+			}else{
+				
+				// LL not empty, need to insert data in order
+				new_Node = create (actual_data , NULL);
+				head = insert(head, new_Node);
+				
+			}
+			
+			
+		}else if (*token == 'd'){
 			//delete
 
 		}else{
--- first.h
+++ first.h
@@ -5,7 +5,7 @@
 	Struct for creating nodes
 */
 typedef
-struct _NODE{
+struct _Node{
 
 	int data;
 	struct _NODE * next;
@@ -16,9 +16,9 @@ struct _NODE{
 /*
 	Function for node creation
 */
-node* create(int data , node* next){
+_Node* create(int data , _Node* next){
 	
-	Node* new_Node = (Node*)malloc(sizeOf(Node)); //Do i need to use _NODE?
+	_Node* new_Node = (_Node*)malloc(sizeOf(_Node)); //Do i need to use _NODE?
 	
 	if(new_Node == NULL){
 		printf("ERROR CREATING NEW NODE...CHECK FUNCTION!!!");
@@ -32,9 +32,82 @@ node* create(int data , node* next){
 /*
 	Function for inserting node in LL
 */
-node* insert(node* head, node* node_to_insert){
+_Node* insert(_Node* head, _Node* node_to_insert){
 	//STEP 1: Check if list already contains node with that value
 	// if so... do not insert...
+	//THIS FUNCTION RETURNS HEAD OF NEW LIST 
+	_Node* ptr = head; // TODO: Do you need to use _NODE since pre-compiler 
+	
+	if( head->data > node_to_insert->data){
+		// node needs to be inserted in front
+		node_to_insert->next = head;
+		return node_to_insert; // returns this to update head
+
+	}else{
+
+		while( ptr != NULL){
+		//scan list if node is contained in list
+		if(ptr.data == node_to_insert.data){
+
+			return head; 
+			//  base case for checking if node is in list
+			// if it is we just return head and DO NOT insert;
+
+		}
+
+			ptr = ptr->next;
+		}
+	}
+	
+	//need to insert if getting to this point 
+	ptr = head; // reset ptr to head 
+	while(ptr != NULL){
+		// since we already have  inserting at head handled outside in main we only have two cases
+		_Node* next = ptr->next;
+		if(ptr->data < node_to_insert->data && node_to_insert->data < next->data){
+			//we need to insert in between two nodes
+			node_to_insert->next = next;
+			ptr->next = node_to_insert;
+			return head; // successful insert
+		}
+		else if( next == NULL){ // checking for insertion at end
+			ptr->next = node_to_insert;
+			return head;
+		}
+		else{
+			ptr = ptr->next;
+		}
+
+
+	}
+}
+_Node* delete(_Node* head, _Node* node_to_delete){
+	
+	_Node* cur = head;
+	_Node* nxt = cur->next;
+
+	while(cur != NULL){
+
+		if(cur->data == node_to_delete->data){
+			//delete head
+			head->next = nxt;
+			cur->next = NULL;
+			return head;
+		}else if(nxt->data == node_to_delete->data){
+			//delete next 
+			cur->next = nxt->next;
+			nxt->next = NULL;
+			return head;
+		}else{
+			cur = nxt;
+			nxt = nxt->next;
+		}
+		//TODO: validate this works 
+}
+
+
+
+
 
 }
 
